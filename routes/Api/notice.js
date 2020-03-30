3efcdb0a1ed417d7d09eb8a2a1cf00797982382b
CAI zhijia <1107088997@qq.com>
2020-03-30 23:48:51 +0800

    update

--- routes/Api/notice.js
+++ routes/Api/notice.js
@@ -50,5 +50,44 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     })
 })
 
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    User.findOne({
+        username: req.user.username
+    }).then((user) => {
+        if (!user) {
+            return res.json({
+                code: '-1',
+                message: '用户信息不存在'
+            })
+        }
+
+        Notice.findOne({
+            user_id: req.user._id
+        }).then(notice => {
+            if (!notice) {
+                return res.json({
+                    code: '-1',
+                    message: '该通知不存在'
+                })
+            }
+
+            var noticeList = notice.NoticeList
+            for (let i = 0; i < noticeList.length; i++) {
+                if (noticeList[i]._id == req.body.id) {
+                    noticeList.splice(i, 1)
+                }
+            }
+
+
+            Notice.findOneAndUpdate({ user_id: req.user._id }, { NoticeList: noticeList }, { new: true }).then(notice => {
+                res.json({
+                    code: '0',
+                    data: notice,
+                    message: 'getNotice successful'
+                })
+            })
+        })
+    })
+})
 
 module.exports = router
